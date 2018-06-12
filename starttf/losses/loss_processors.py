3393472df7e2430da4b0a2146259dd7e360bc40d
Michael Fürst <mail@michaelfuerst.de>
2018-06-12 10:40:16 +0200

    Added multiloss, interpolation loss and batch alpha balancing.

--- starttf/losses/loss_processors.py
+++ starttf/losses/loss_processors.py
@@ -35,7 +35,49 @@ def focus_loss(labels, probs, loss, gamma):
         # Compute p_t that is used in paper.
         # FIXME is it possible that the 1-p term does not make any sense?
         p_t = tf.reduce_sum(probs * labels, axis=-1)# + tf.reduce_sum((1.0 - probs) * (1.0 - labels), axis=-1)
-        return tf.pow(1.0 - p_t, gamma) * loss
+
+        focal_factor = tf.pow(1.0 - p_t, gamma) if gamma > 0 else 1  # Improve stability for gamma = 0
+        return focal_factor * loss
+
+
+def interpolate_loss(labels, loss1, loss2, interpolation_values):
+    """
+    Interpolate two losses linearly.
+
+    :param labels: A float tensor of shape [batch_size, ..., num_classes] representing the label class probabilities.
+    :param loss1: A float tensor of shape [batch_size, ...] representing the loss1 for interpolation.
+    :param loss2: A float tensor of shape [batch_size, ...] representing the loss2 for interpolation.
+    :param interpolation_values: The values for each class how much focal loss should be interpolated in.
+    :return: A tensor representing the weighted cross entropy.
+    """
+    with tf.variable_scope("interpolate_focus_loss"):
+        # Select the probs or weights with the labels.
+        t = tf.reduce_sum(labels * interpolation_values, axis=-1)
+        return (1 - t) * loss1 + t * loss2
+
+
+def batch_alpha_balance_loss(labels, loss):
+    """
+    Calculate the alpha balanced cross_entropy.
+
+    This means for each sample the cross entropy is calculated and then weighted by the class specific weight.
+
+    :param labels: A float tensor of shape [batch_size, ..., num_classes] representing the label class probabilities.
+    :param loss: A float tensor of shape [batch_size, ...] representing the loss that should be focused.
+    :param alpha_weights: A float tensor of shape [1, ..., num_classes] (... is filled with ones to match number
+                              of dimensions to labels tensor) representing the weights for each class.
+    :return: A tensor representing the weighted cross entropy.
+    """
+    with tf.variable_scope("batch_alpha_balance"):
+        # Compute the occurrence probability for each class
+        mu, _ = tf.nn.moments(labels, [0, 1, 2])
+
+        # For weighting a class should be down weighted by its occurrence probability.
+        not_mu = 1 - mu
+
+        # Select the class specific not_mu
+        not_mu_class = tf.reduce_sum(labels * not_mu, axis=-1)
+        return not_mu_class * loss
 
 
 def mask_loss(input_tensor, binary_tensor):
@@ -50,3 +92,21 @@ def mask_loss(input_tensor, binary_tensor):
         mask = tf.cast(tf.cast(binary_tensor, tf.bool), tf.float32)
 
         return input_tensor * mask
+
+
+def multiloss(losses):
+    """
+    Create a loss from multiple losses my mixing them.
+    This multi-loss implementation is inspired by the Paper "Multi-Task Learning Using Uncertainty to Weight Losses
+    for Scene Geometry and Semantics" by Kendall, Gal and Cipolla.
+    :param losses: A list containing all losses that should be merged.
+    :return: A single loss.
+    """
+    with tf.variable_scope("multiloss"):
+        sum_loss = 0
+        for i, loss in enumerate(losses):
+            with tf.variable_scope(str(i)) as scope:
+                sigma = tf.get_variable(name="sigma", dtype=tf.float32, initializer=tf.constant(1.0), trainable=True)
+                sigma_2 = tf.pow(sigma, 2)
+                sum_loss += 0.5 / sigma_2 * loss + tf.log(sigma_2 + 1.0)
+        return sum_loss
